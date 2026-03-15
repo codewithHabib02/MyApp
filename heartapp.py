85475ea93175482ecdb95d2027d2f998f4e38b48
Habibulie M. Barry <mhabibulie02@gmail.com>
2026-03-16 02:42:00 +0300

    Update heartapp.py

--- heartapp.py
+++ heartapp.py
@@ -41,8 +41,8 @@ if not st.session_state["login"]:
 # -----------------------------
 # LOAD DATA AND MODEL
 # -----------------------------
-df=pd.read_excel("Heart/Book1.xlsx")
-lr = pd.read_csv("Heart/heart.csv")
+df=pd.read_excel("Book1.xlsx")
+lr = pd.read_csv("heart.csv")
 X = lr.drop("target", axis=1)
 
 model = joblib.load("linear.pkl")
